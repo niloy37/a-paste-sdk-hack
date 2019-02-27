78a4a6d3838146e6cf1fb64d91d685d103ce9644
alex <alex@coqui.me>
2019-02-27 14:40:26 -0600

    cycumber variables

--- string cheese/misc/variables.h
+++ string cheese/misc/variables.h
@@ -1,47 +1,48 @@
 #pragma once
 #include <string>
+
+
+#define variablespace namespace
+variablespace ap::settings {
 #define hibbity true
 #define cucumber false
 #define varsetting(type, name, val) type name = val
-#define variablespace namespace
-variablespace ap::settings {
+#define boolin static bool
 
-
-
-		varsetting(static bool, ribbit, cucumber);
+		varsetting(boolin, ribbit, cucumber);
 
 		/* anti-aim */
 
-		varsetting(static bool, slide_walk, cucumber);
+		varsetting(boolin, slide_walk, cucumber);
 
 		/* visuals */
 
-		varsetting(static bool, name_esp, hibbity);
-		varsetting(static bool, health_esp, hibbity);
-		varsetting(static bool, esp_corner_box, hibbity);
-		varsetting(static bool, backtrack_stick_figure, hibbity);
-		varsetting(static bool, dont_render_team, hibbity);
-		varsetting(static bool, armour_flags, hibbity);
-		varsetting(static bool, visualize_backtrack_dots, hibbity);
-		varsetting(static bool, visualize_backtrack_octagon, cucumber);
-		varsetting(static bool, no_scope_lines, hibbity);
-		varsetting(static bool, world_modulation, cucumber);
-		varsetting(static bool, snap_lines, cucumber);
-		varsetting(static bool, remove_smoke, hibbity);
-		varsetting(static bool, no_flash, hibbity);
-		varsetting(static bool, force_crosshair, hibbity);
-		varsetting(static bool, render_custom_crosshair, cucumber);
+		varsetting(boolin, name_esp, hibbity);
+		varsetting(boolin, health_esp, hibbity);
+		varsetting(boolin, esp_corner_box, hibbity);
+		varsetting(boolin, backtrack_stick_figure, hibbity);
+		varsetting(boolin, dont_render_team, hibbity);
+		varsetting(boolin, armour_flags, hibbity);
+		varsetting(boolin, visualize_backtrack_dots, hibbity);
+		varsetting(boolin, visualize_backtrack_octagon, cucumber);
+		varsetting(boolin, no_scope_lines, hibbity);
+		varsetting(boolin, world_modulation, cucumber);
+		varsetting(boolin, snap_lines, cucumber);
+		varsetting(boolin, remove_smoke, hibbity);
+		varsetting(boolin, no_flash, hibbity);
+		varsetting(boolin, force_crosshair, hibbity);
+		varsetting(boolin, render_custom_crosshair, cucumber);
 
 		/* legit-aim */
 
-		varsetting(static bool, legit_backtrack, hibbity);
-
-		/* misc */
-
-		varsetting(static bool, auto_jump, hibbity);
-		varsetting(static bool, legit_auto_strafer, hibbity);
-		varsetting(static bool, no_crouch_cooldown, hibbity);
-		varsetting(static bool, fast_crouch_loop, cucumber);
-		varsetting(static bool, set_spotted, hibbity);
+		varsetting(boolin, legit_backtrack, hibbity);
+				   
+		/* misc */ 
+				   
+		varsetting(boolin, auto_jump, hibbity);
+		varsetting(boolin, legit_auto_strafer, hibbity);
+		varsetting(boolin, no_crouch_cooldown, hibbity);
+		varsetting(boolin, fast_crouch_loop, cucumber);
+		varsetting(boolin, set_spotted, hibbity);
 
 }

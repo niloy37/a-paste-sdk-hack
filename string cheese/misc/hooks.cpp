47074d135a5833b5b2ff14b0a0a8c8707a8cf2a9
alex <alex@coqui.me>
2019-03-08 13:59:19 -0600

    fixed laggs

--- string cheese/misc/hooks.cpp
+++ string cheese/misc/hooks.cpp
@@ -141,20 +141,19 @@ namespace
 		ap::sdk::c_material* bus_down_ghost_flames = ap::interfaces::material_system->find_material("models/inventory_items/dreamhack_trophies/dreamhack_star_blur", nullptr);
 		ap::sdk::c_material* ghost_flash = ap::interfaces::material_system->find_material("models/inventory_items/dogtags/dogtags_outline", nullptr);
 		ap::sdk::c_material* spooky_ghost = ap::interfaces::material_system->find_material("models/inventory_items/dogtags/dogtags_lightray", nullptr);
+		ap::sdk::c_material* spooky_ghost2 = ap::interfaces::material_system->find_material("models/inventory_items/dogtags/dogtags_lightray", nullptr);
 		ap::sdk::c_material* ghost_flames = ap::interfaces::material_system->find_material("models/extras/speech_info", nullptr);
-		ap::sdk::c_material* red_glow_i_think = ap::interfaces::material_system->find_material("vgui/achievements/glow", nullptr);
-		ap::sdk::c_material* wire_frame = ap::interfaces::material_system->find_material("models/wireframe", nullptr);
 		ap::sdk::c_material* halo_add = ap::interfaces::material_system->find_material("materials/dev/halo_add_to_screen", nullptr);
 
 		if (ap::interfaces::engine->is_connected() && ap::interfaces::engine->is_in_game()) {
 			spooky_ghost->color_modulate(ap::rgbaf::RED());
 			spooky_ghost->set_material_var_flag(ap::sdk::MATERIAL_VAR_WIREFRAME, true);
 			animated_darude->color_modulate(ap::rgbaf::RED());
-			
+			//spooky_ghost->set_material_var_flag(ap::sdk::MATERIAL_VAR_IGNOREZ, true);
 			if (ap::settings::hand_chams) {
 				if (strstr(model_name, "arms")) {
 					ap::interfaces::render_view->set_blend(1.f);
-					ap::interfaces::model_render->forced_material_override(spooky_ghost);
+					ap::interfaces::model_render->forced_material_override(spooky_ghost2);
 					original_draw_model_execute(ecx, context, state, render_info, matrix);
 					ap::interfaces::render_view->set_blend(1.f);
 					ap::interfaces::model_render->forced_material_override(animated_darude);
@@ -163,6 +162,9 @@ namespace
 			}
 			if (ap::settings::enemy_chams) {
 				if (strstr(model_name, "player")) {
+					ap::interfaces::render_view->set_blend(1.f);
+					ap::interfaces::model_render->forced_material_override(spooky_ghost2);
+					original_draw_model_execute(ecx, context, state, render_info, matrix);
 					ap::interfaces::render_view->set_blend(1.f);
 					ap::interfaces::model_render->forced_material_override(spooky_ghost);
 					original_draw_model_execute(ecx, context, state, render_info, matrix);

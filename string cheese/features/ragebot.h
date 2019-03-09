1363c5cbe100c019e4038ebd23d6765d1c6e66f3
fatmanjoieakabrandonm <47487930+fatmanjoieakabrandonm@users.noreply.github.com>
2019-03-09 00:03:22 +0000

    upd
    upd

--- string cheese/features/ragebot.h
+++ string cheese/features/ragebot.h
@@ -1,4 +1,26 @@
 #pragma once
+#include <list>
+#include <algorithm>
+#include "../misc/defines.h"
+#include "../misc/vector.h"
+#include "../misc/interfaces.h"
+#include "../sdk/c_client_entity_list.h"
+#include "../sdk/c_engine.h"
+#include "../sdk/c_base_entity.h"
+#include "../misc/utils.h"
+#include "../sdk/c_client_state.h"
+#include "../sdk/c_globals.h"
+#include "../sdk/c_player_info.h"
+#include "../features/backtrack.h"
+#include "../misc/renderer.h"
+#include "../sdk/c_cvar.h"
+#include "../misc/variables.h"
+#include "../misc/singleton.h"
+#include "../sdk/c_trace.h"
+#include "../sdk/c_base_weapon.h"
+#include "../misc/globalvars.h"
+#include "../features/autowall.h"
+#include "../sdk/c_cvar.h"
 
 namespace ap::features::ragebot {
 	void drop_target();

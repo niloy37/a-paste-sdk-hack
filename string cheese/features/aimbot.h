bc9705e0c3cdcd7a0fa5a6c0d7be558079d86693
alex <alex@coqui.me>
2019-04-11 19:32:10 -0500

    pp

--- string cheese/features/aimbot.h
+++ string cheese/features/aimbot.h
@@ -1,1 +1,16 @@
 #pragma once
+#include "../misc/vector.h"
+#include "../sdk/c_base_entity.h"
+namespace ap::features::ragebot {
+	void drop_target();
+	void run();
+	void on_create_move();
+	int target_index = -1;
+	float best_distance = 8192.f;
+	bool aimbotted_in_current_tick;
+	bool fired_in_that_tick;
+	float current_aim_simulationtime;
+	vec3f current_aim_position;
+	vec3f current_aim_player_origin;
+	ap::sdk::c_base_entity* pTarget;
+}

29d93dc1fa53178f550819de20fcab0d88cca36f
alex <alex@coqui.me>
2019-04-29 19:15:50 -0500

    included ragebot stuff just need to convert it when I got time

--- string cheese/features/aimbot.cpp
+++ string cheese/features/aimbot.cpp
@@ -1,24 +1,139 @@
-#include <list>
-#include <algorithm>
-#include "../misc/defines.h"
-#include "../misc/vector.h"
-#include "../misc/interfaces.h"
-#include "../sdk/c_client_entity_list.h"
-#include "../sdk/c_engine.h"
-#include "../sdk/c_base_entity.h"
-#include "../misc/utils.h"
-#include "../sdk/c_client_state.h"
-#include "../sdk/c_globals.h"
-#include "../sdk/c_player_info.h"
-#include "../features/backtrack.h"
-#include "../misc/renderer.h"
-#include "../sdk/c_cvar.h"
 #include "aimbot.h"
-#include "../misc/globalvars.h"
+#include <algorithm>
+
+#include "../ap_core.h"
+			
+#include "../misc/interfaces.h"
+#include "../misc/vector.h"
+#include "../misc/utils.h"
+#include "../misc/defines.h"
+
+#include "../sdk/c_engine.h"
+#include "../sdk/c_client_entity_list.h"
+#include "../sdk/c_base_entity.h"
+#include "../sdk/c_base_weapon.h"
+#include "../sdk/c_user_cmd.h"
 #include "../sdk/c_trace.h"
-#include "../menu.h"
+#include "../sdk/c_client_class.h"
+#include "../sdk/c_physics_props.h"
+#include "../sdk/surface_data.h"
+#include "../misc/defines.h"
 
 namespace ap::features::aimbot {
-
+//	void aimbot::create_move() {
+//		ap::interfaces::engine->get_viewangles(m_engine_angles);
+//		m_local_weapon = g_ctx.m_local->m_hActiveWeapon().Get();
+//
+//		auto weapon_recoil_scale = g_csgo.m_cvar()->FindVar("weapon_recoil_scale");
+//		m_recoil_scale = weapon_recoil_scale->GetFloat();
+//
+//		if (!m_local_weapon)
+//			return;
+//
+//		update_config();
+//
+//		if ((g_ctx.m_local->m_flNextAttack() > util::server_time()))
+//			return;
+//
+//		if (m_local_weapon->is_non_aim() || m_local_weapon->m_iClip1() < 1)
+//			return;
+//
+//		if (!m_local_weapon->can_fire())
+//			return;
+//
+//		for (int i = 1; i <= g_csgo.m_globals()->m_maxclients; i++) {
+//			auto e = static_cast<player_t*>(g_csgo.m_entitylist()->GetClientEntity(i));
+//
+//			if (!e->valid(true))
+//				continue;
+//
+//			ap::matrix3x4_t matrix[128];
+//		}
+//	}
+//
+//	void aimbot::update_config() {
+//		auto id = m_local_weapon->m_iItemDefinitionIndex();
+//
+//		int element = -1;
+//
+//		if (m_local_weapon->is_pistol() && id != WEAPON_DEAGLE)
+//			element = 0;
+//
+//		if (m_local_weapon->m_iItemDefinitionIndex() == WEAPON_DEAGLE)
+//			element = 1;
+//
+//		if (m_local_weapon->is_smg())
+//			element = 3;
+//
+//		if (id == WEAPON_AWP || id == WEAPON_SSG08)
+//			element = 4;
+//
+//		if (id == WEAPON_SCAR20 || id == WEAPON_G3SG1)
+//			element = 5;
+//
+//		if (m_local_weapon->is_shotgun())
+//			element = 6;
+//
+//		if (element == -1)
+//			element = 2;
+//
+//		config.m_autoshoot = g_cfg.ragebot.autoshoot[element];
+//		config.m_autowall = g_cfg.ragebot.autowall[element];
+//		config.m_minimum_damage = g_cfg.ragebot.minimum_damage[element];
+//		config.m_hitscan = g_cfg.ragebot.hitscan[element];
+//		config.m_hitscan_history = g_cfg.ragebot.hitscan_history[element];
+//		config.m_multipoint = g_cfg.ragebot.multipoint_hitboxes[element];
+//		config.m_pointscale = g_cfg.ragebot.pointscale[element];
+//		config.m_hitchance = g_cfg.ragebot.hitchance[element];
+//		config.m_hitchance_amount = g_cfg.ragebot.hitchance_amount[element];
+//		config.m_baim = g_cfg.ragebot.baim_settings[element];
+//		config.m_quickstop = g_cfg.ragebot.quickstop[element];
+//
+//		config.m_autoscope = false;
+//
+//		if (element == 4 || element == 5)
+//			config.m_autoscope = g_cfg.ragebot.autoscope[element - 3];
+//		else if (element == 2 && (id == WEAPON_AUG || id == WEAPON_SG556))
+//			config.m_autoscope = g_cfg.ragebot.autoscope[0];
+//	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- string cheese/features/aimbot.h
+++ string cheese/features/aimbot.h
@@ -1,11 +1,37 @@
 #pragma once
-#include "../misc/vector.h"
-#include "../sdk/c_base_entity.h"
-#include "../sdk/c_user_cmd.h"
-#include "../menu.h"
-#include "../misc/math.h"
 #include "../misc/singleton.h"
+
 namespace ap::features::aimbot {
-	
-	
+	class aimbot : public singleton< aimbot > {
+		//private:
+		//	struct aimbotconfig_t {
+		//		bool m_autoshoot;
+		//		bool m_autowall;
+		//		bool m_autoscope;
+		//		bool m_hitchance;
+		//
+		//		int m_minimum_damage;
+		//		int m_hitchance_amount;
+		//
+		//		std::vector< MultiDropdownItem_t > m_hitscan;
+		//		std::vector< MultiDropdownItem_t > m_hitscan_history;
+		//		std::vector< MultiDropdownItem_t > m_multipoint;
+		//		std::vector< MultiDropdownItem_t > m_baim;
+		//
+		//		float m_pointscale;
+		//
+		//		bool m_quickstop;
+		//	}config;
+		//
+		//	weapon_t * m_local_weapon;
+		//	vec3_t m_engine_angles;
+		//	float m_recoil_scale;
+		//	player_t * m_target;
+		//
+		//	std::deque< tickrecord_t > m_track;
+		//public:
+		//	void create_move( );
+		//
+		//	void update_config( );
+	};
 }

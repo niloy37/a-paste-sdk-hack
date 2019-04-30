1c5694a5166dcd68d39b06264dabdc5b146d9f6a
alex <alex@coqui.me>
2019-04-29 20:15:47 -0500

    nigwardo

--- string cheese/features/aimbot.h
+++ string cheese/features/aimbot.h
@@ -1,37 +1,40 @@
 #pragma once
 #include "../misc/singleton.h"
+#include "../sdk/c_base_entity.h"
+#include "../sdk/c_base_weapon.h"
+#include <deque>
 
 namespace ap::features::aimbot {
 	class aimbot : public singleton< aimbot > {
-		//private:
-		//	struct aimbotconfig_t {
-		//		bool m_autoshoot;
-		//		bool m_autowall;
-		//		bool m_autoscope;
-		//		bool m_hitchance;
-		//
-		//		int m_minimum_damage;
-		//		int m_hitchance_amount;
-		//
-		//		std::vector< MultiDropdownItem_t > m_hitscan;
-		//		std::vector< MultiDropdownItem_t > m_hitscan_history;
-		//		std::vector< MultiDropdownItem_t > m_multipoint;
-		//		std::vector< MultiDropdownItem_t > m_baim;
-		//
-		//		float m_pointscale;
-		//
-		//		bool m_quickstop;
-		//	}config;
-		//
-		//	weapon_t * m_local_weapon;
-		//	vec3_t m_engine_angles;
-		//	float m_recoil_scale;
-		//	player_t * m_target;
-		//
-		//	std::deque< tickrecord_t > m_track;
-		//public:
-		//	void create_move( );
-		//
-		//	void update_config( );
+		private:
+			struct aimbotconfig_t {
+				bool m_autoshoot;
+				bool m_autowall;
+				bool m_autoscope;
+				bool m_hitchance;
+		
+				int m_minimum_damage;
+				int m_hitchance_amount;
+		
+				//std::vector< MultiDropdownItem_t > m_hitscan;
+				//std::vector< MultiDropdownItem_t > m_hitscan_history;
+				//std::vector< MultiDropdownItem_t > m_multipoint;
+				//std::vector< MultiDropdownItem_t > m_baim;
+		
+				float m_pointscale;
+		
+				bool m_quickstop;
+			}config;
+		
+			ap::sdk::c_base_weapon * m_local_weapon;
+			ap::vec3f m_engine_angles;
+			float m_recoil_scale;
+			ap::sdk::c_base_entity * m_target;
+		
+			//std::deque< tickrecord_t > m_track;
+		public:
+			void create_move( );
+		
+			void update_config( );
 	};
 }
--- string cheese/features/backtrack.cpp
+++ string cheese/features/backtrack.cpp
@@ -20,40 +20,9 @@
 backtrackspace ap::features::backtrack {
 
 
-	struct sequence_data {
+	
 
-		sequence_data(int instate, int outstate, int seqnr, float realtime) {
 
-			inreliablestate = instate;
-			outreliablestate = outstate;
-			sequencenr = seqnr;
-			time = realtime;
-		}
-
-		int inreliablestate, outreliablestate, sequencenr;
-		float time;
-
-	};
-
-	struct vars_bt {
-
-		int latest_tick;
-		int last_sequence;
-		float interval_per_tick;
-	};
-
-	struct backtrack_data {
-
-		float simtime;
-		vec3f hitbox_position;
-		int tick_count;
-		matrix3x4_t bone_matrix[128];
-	};
-
-	/* externs */
-	extern backtrack_data bone_data[64][12];
-	extern backtrack_data entity_data[64][12];
-	extern std::list<sequence_data> sequences;
 
 	/* pointers */
 	vars_bt variables;
